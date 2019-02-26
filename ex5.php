358a3fa5ab40d01e4006749b6166b0c7344f1189
1831402 <1831402@BiblioMaq06.itp.local>
2019-02-26 14:08:14 -0300

    ex5

--- ex5.php
+++ ex5.php
@@ -7,16 +7,21 @@
     <body>
         <?php
             $posi = array();
-            $posi[0]= 'Branca de Neve';
-            $posi[1]= ''
-            $posi[2]=
-            $posi[3]=
-            $posi[4]=
-            $posi[5]=
-            $posi[6]=
-            $posi[7]=
-            $posi[8]=
-            $posi[9]=
+            $posi[0]= 'Princesas';
+            $posi[1]= 'Cinderela';
+            $posi[2]= 'Rapunzel';
+            $posi[3]= 'Moana';
+            $posi[4]= 'Branca de Neve';
+            $posi[5]= 'Mulan';
+            $posi[6]= 'Elsa';
+            $posi[7]= 'Bela';
+            $posi[8]= 'Aurora';
+            $posi[9]= 'Valente';
         ?>
+        <select>
+            <?php for ($i=0; $i<=9; $i++;) { ?>
+            <option><?php echo $posi[$i] ?></option>
+            <?php } ?>
+        </select>
     </body>
 </html>
